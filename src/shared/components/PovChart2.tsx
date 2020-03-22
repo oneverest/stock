19f60eb1f511b8283ca930ddf6fdbc8f21c99755
xieyu <4361605@qq.com>
2020-03-22 10:41:12 +0800

    fix

---
+++ src/shared/components/PovChart2.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Ref } from 'semantic-ui-react';
+import Chart from 'chart.js';
+import 'chart.js/dist/Chart.css';
+
+interface Props {
+  width?: number;
+  height?: number;
+  net_worth?: number[];
+  position_ratio?: number[];
+  date?: number[];
+}
+
+export default function PovChart2({
+  width = 400,
+  height = 400,
+  net_worth = [],
+  position_ratio = [],
+  date = [],
+}: Props) {
+  const canvasRef = React.useRef();
+
+  React.useEffect(() => {
+    console.log(canvasRef.current);
+    let chart: any = null;
+    if (canvasRef.current) {
+      const ctx = (canvasRef.current as any).getContext('2d');
+      if (ctx) {
+        chart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: date,
+            datasets: [
+              {
+                label: '资产净值',
+                data: net_worth,
+                order: 1,
+                type: 'line',
+                borderColor: 'red',
+                backgroundColor: 'rgba(255, 255, 255, 0)',
+                borderWidth: 2,
+              },
+              {
+                label: '持仓比例',
+                data: position_ratio,
+                order: 2,
+                backgroundColor: 'rgba(54, 162, 235, 1)',
+              },
+            ],
+          },
+          options: {
+            scales: {
+              xAxes: [
+                {
+                  type: 'time',
+                  time: {
+                    unit: 'day',
+                    displayFormats: {
+                      day: 'MM-DD',
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        });
+      }
+    }
+  });
+
+  return (
+    <Ref innerRef={canvasRef}>
+      <canvas></canvas>
+    </Ref>
+  );
+}
--- src/shared/routes/agent/routes/pov/stat/index.tsx
+++ src/shared/routes/agent/routes/pov/stat/index.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
-import PovChart from 'components/PovChart';
+import React, { useEffect, useReducer } from 'react';
+// import PovChart from 'components/PovChart';
+import PovChart from 'components/PovChart2';
+import { getAllPovs } from 'services/pov';
+
+const initialState = { net_worth: [], position_ratio: [], date: [] };
+
+const reducer = (state: any, action: any) => {
+  switch (action.type) {
+    case 'render':
+      return { ...state, ...action.payload };
+    default:
+      throw new Error('unknown action type: ' + action.type);
+  }
+};
 
 export function StatRoute() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { net_worth, position_ratio, date } = state;
+  useEffect(() => {
+    getAllPovs({ page: 1, pageSize: 300 }).then(result => {
+      if (result.isSuccess) {
+        const { data } = result.getValue() as any;
+        if (data.length !== date.length) {
+          dispatch({
+            type: 'render',
+            payload: {
+              net_worth: data.map((item: any) => item.net_worth),
+              position_ratio: data.map((item: any) => item.position_ratio),
+              date: data.map((item: any) => item.record_date),
+            },
+          });
+        }
+      }
+    });
+  });
   return (
     <React.Fragment>
-      <PovChart />
+      <PovChart net_worth={net_worth} position_ratio={position_ratio} date={date} />
     </React.Fragment>
   );
 }
